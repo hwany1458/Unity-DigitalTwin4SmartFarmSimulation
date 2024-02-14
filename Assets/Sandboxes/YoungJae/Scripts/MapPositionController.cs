d3c094f64a84925843eba1751770b9eb2df58a89
co2plant <93631841+co2plant@users.noreply.github.com>
2024-02-14 14:31:21 +0900

    ADD :: 날씨 정보 확인 UI
    + 공공데이터 포털 기상청 단기예보 조회서비스 API를 활용한 날씨 조회 서비스 구현 및 추가
    + TMP_pro 추가
    
    OS : macOS Sonoma 14.3

---
+++ Assets/Sandboxes/YoungJae/Scripts/MapPositionController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+public class MapPositionController : MonoBehaviour
+{
+}
